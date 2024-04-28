d39f9f56733754626e6696d552759c2dd2045f19
Khant Htut Zin <khanthtutzin@gmail.com>
2024-04-28 23:35:01 +0630

    work experience section

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -32,26 +32,28 @@ import CanvasLoader from '../Loader';
 
 
 const ComputersCanvas = () => {
-
   const [isMobile, setIsMobile] = useState(false);
 
   useEffect(() => {
+    // Add a listener for changes to the screen size
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
 
-    const mediaQuery = window.matchMedia('(max-width: 500px)');
-
+    // Set the initial value of the `isMobile` state variable
     setIsMobile(mediaQuery.matches);
 
+    // Define a callback function to handle changes to the media query
     const handleMediaQueryChange = (event) => {
-      setIsMobile(event,matches);
-    }
+      setIsMobile(event.matches);
+    };
 
-    mediaQuery.addEventListener('change',handleMediaQueryChange);
+    // Add the callback function as a listener for changes to the media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
 
+    // Remove the listener when the component is unmounted
     return () => {
-      mediaQuery.addEventListener('change',handleMediaQueryChange);
-    }
-
-  },[])
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
 
 
     return (
